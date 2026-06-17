04d350721b231c6c0fe02d147553c8ca9e2a76a3
Gilvan Pedro <87739497+GilvanPedro@users.noreply.github.com>
2026-06-17 20:10:57 -0300

    Update InstalarAplicativos.bat

--- InstalarAplicativos.bat
+++ InstalarAplicativos.bat
@@ -1,43 +1,41 @@
 @echo off
-:: Corrigir acentos (UTF-8)
-chcp 65001 >nul
+cls
 
-:: ==========================================
-::     VERIFICAÇÃO E ELEVAÇÃO DE PRIVILÉGIOS
-:: ==========================================
+:: =======================================
+:: VERIFICACAO E ELEVACAO DE PRIVILEGIOS
+:: =======================================
 net session >nul 2>&1
 if %errorLevel% neq 0 (
-    echo Solicitando privilégios de Administrador...
+    echo Solicitando privilegios de Administrador...
     powershell -NoProfile -ExecutionPolicy Bypass -Command "Start-Process '%~f0' -Verb RunAs"
     exit /b
 )
 
-title Gerenciador de Instalações - Winget
+title Gerenciador de Instalacoes - Winget
 cls
-echo ==========================================
-echo             INICIANDO INSTALAÇÕES
-echo ==========================================
+echo =======================================
+echo           INICIANDO INSTALACOES
+echo =======================================
 echo.
 
-:: ==========================================
-::              LISTA DE APLICATIVOS
-:: ==========================================
+:: =======================================
+:: LISTA DE APLICATIVOS
+:: =======================================
 set "apps=Zoom.Zoom Mozilla.Firefox.pt-BR VideoLAN.VLC Google.Chrome Adobe.Acrobat.Reader.64-bit TheDocumentFoundation.LibreOffice 7zip.7zip Microsoft.Edge CodecGuide.K-LiteCodecPack.Standard"
 
-:: ==========================================
-::               LOOP DE INSTALAÇÃO
-:: ==========================================
+:: =======================================
+:: LOOP DE INSTALACAO
+:: =======================================
 for %%a in (%apps%) do (
     echo [PROCESSO] Tentando instalar: %%a
     
-    :: Comando alterado para 'winget install' com parâmetros de automação e aceitação de termos
     winget install --id "%%a" -e --silent --force --accept-source-agreements --accept-package-agreements
     
     if %errorLevel% equ 0 (
         echo [OK] Sucesso ao instalar: %%a
     ) else (
-        echo [ERRO] Falha ao instalar %%a (Código: %errorLevel%)
+        echo [ERRO] Falha ao instalar %%a (Codigo: %errorLevel%)
     )
-    echo ------------------------------------------
+    echo ---------------------------------------
 )
 pause
